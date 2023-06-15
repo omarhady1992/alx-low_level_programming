5546f80696a3a65ffded786bbf7585c2bf3fe3ed
root <ohady1992@gmail.com>
2023-06-15 02:30:02 -0700

    added 4-puts.c : That prints a phrase on the screen

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - puts the phrase Programming is like building a multilingual puzzle on the screen
+ * Return: 0 if exited properly
+ */
+
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
