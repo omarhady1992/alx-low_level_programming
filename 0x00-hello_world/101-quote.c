4e9ec04c0d50077c937dba0e90402cbdd572622f
root <ohady1992@gmail.com>
2023-06-15 04:42:13 -0700

    100-intel:generates assembly code and 101-quote.c:prints a code with Return 1

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main-prints a quote using fputs
+ * Return: 1 if it is executed
+ */
+
+int main(void)
+{
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
+}
