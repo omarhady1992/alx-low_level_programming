d1b3de558e8bec8f24e50a76ded35a5a959bb3cb
root <ohady1992@gmail.com>
2023-06-20 04:12:29 -0700

    bug fixes

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,2 +1,13 @@
-int _putchar(char alpha);
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ * File: main.h
+ *
+ * Desc: Header file containing declartions for all function
+ */
+
+int _putchar(char c);
 void print_alphabet(void);
+
+#endif
