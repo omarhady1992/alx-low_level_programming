5d4e5718cfee396246e3bd47c203ca3cec9bf6e0
root <ohady1992@gmail.com>
2023-06-20 13:47:40 -0700

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -13,10 +13,10 @@ int print_last_digit(int n)
 	if (n > 0)
 		n = -n;
 	a = n % 10;
-	if (a < 0)
+	if (rem < 0)
 		a = -a;
 
-	_putchar(a + 48);
+	_putchar(rem + 48);
 
-	return (a);
+	return (rem);
 }
