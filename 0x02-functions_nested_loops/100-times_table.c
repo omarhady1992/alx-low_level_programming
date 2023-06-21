9a4539db81a5d7673b3edf04deb1e7a09cc11633
root <ohady1992@gmail.com>
2023-06-21 01:14:25 -0700

    fix 100-times_table.c bug

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -4,6 +4,7 @@
  * print_times_table-prints the times table to n number
  * @n: the number to which times tables will be printed
  */
+
 void print_times_table(int n)
 {
 	int i, j, k;
@@ -12,27 +13,32 @@ void print_times_table(int n)
 	{
 		for (i = 0; i <= n; i++)
 		{
-			for (j = 0; j < n; j++)
+			for (j = 0; j <= n; j++)
 			{
 				k = j * i;
 				if (j == 0)
 				{
 					_putchar(k + '0');
-				}
-
-				if (k < n && j != 0)
+				} else if (k < 10 && j != 0)
 				{
 					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
 					_putchar(' ');
 					_putchar(k + '0');
-				} else if (k >= n)
+				} else if (k >= 10 && k < 100)
 				{
 					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
 					_putchar((k / 10) + '0');
+					_putchar((k % 10) + '0');
+				} else if (k >= 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((k / 100) + '0');
+					_putchar(((k / 10) % 10) + '0');
 					_putchar((k % 10) + '0');
 				}
 			}
@@ -40,4 +46,3 @@ void print_times_table(int n)
 		}
 	}
 }
-
