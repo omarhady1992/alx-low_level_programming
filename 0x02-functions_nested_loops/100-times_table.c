74a96b4ad51b3fcb469d2135c5e8f66a3bd136e4
root <ohady1992@gmail.com>
2023-06-21 01:08:59 -0700

    Revert "100-times_table.c"
    This reverts commit c328a10909cdbb10793f928ffc8521014b033cfd.

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -5,7 +5,6 @@
  * @n: the number to which times tables will be printed
  * Return: 0 always
  */
-
 void print_times_table(int n)
 {
 	int i, j, k;
@@ -21,27 +20,22 @@ void print_times_table(int n)
 				{
 					_putchar(k + '0');
 				}
-				if (k < 10 && j != 0)
+
+				if (k < n && j != 0)
 				{
 					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
 					_putchar(' ');
 					_putchar(k + '0');
-				} else if (k >= 10 && k < 100)
+				} else if (k >= n)
 				{
 					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
 					_putchar((k / 10) + '0');
 					_putchar((k % 10) + '0');
-				} else if (k >= 100)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar((k / 100) + '0');
-					_putchar(((k / 10) % 10) + '0');
-					_putchar((k % 10) + '0');
+				}
 			}
 			_putchar('\n');
 		}
