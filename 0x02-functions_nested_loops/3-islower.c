b76b737243c922d8c88b8c42f63943c4ae695fa5
root <ohady1992@gmail.com>
2023-06-20 23:46:56 -0700

    Added 3-islower.c and added function _islower to main.h)

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _islower-checks if the character is lowercase
+ * Return: 1 if the input is lower case and 0 if not
+ */
+
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
