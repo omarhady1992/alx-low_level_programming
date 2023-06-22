f5c63b6f0a2e5b37a3c76d798430a3764af3cc5c
root <ohady1992@gmail.com>
2023-06-22 05:09:03 -0700

    fixed 3-print_numbers.c style

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,12 +3,12 @@
 /**
  * print_numbers-a function that prints the numbers, from 0 to 9,
  * follows by a new line
- * Return: 0  always
  */
 
 void print_numbers(void)
 {
 	int a = 0;
+
 	while (a <= 9)
 	{
 		_putchar(a + '0');
