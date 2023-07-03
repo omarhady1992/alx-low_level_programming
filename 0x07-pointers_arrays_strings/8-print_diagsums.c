a37447fd563790b169aa5c5d5467a61b5f474638
root <ohady1992@gmail.com>
2023-07-03 07:49:06 -0700

    7 and 8

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_diagsums- pints sums of the diagonals
+ * @a: the array
+ * @size: array size
+ * Return: nothing
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int i;
+	unsigned int sum_right = 0, sum_left = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		sum_left += a[size * i + i];
+		sum_right += a[(size * (i + 1)) - (i + 1)];
+	}
+	printf("%d, %d\n", sum, sum1);
+}
+
