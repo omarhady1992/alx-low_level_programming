c9b497695d8b35874b1599ebd9bd2b3e4e7b45e1
root <ohady1992@gmail.com>
2023-07-12 06:33:18 -0700

    doc style fixing

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 /**
- * *strdup-a function that returns a pointer 
+ * *_strdup-a function that returns a pointer
  * to a newly allocated space in memory
  * which contains a copy of the string given as a parameter.
  * @str: string to be copied
