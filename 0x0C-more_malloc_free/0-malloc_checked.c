5f23003622d29f970cec8d15af853338898280d7
root <ohady1992@gmail.com>
2023-07-13 15:18:12 -0700

    bug fixes

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -4,7 +4,7 @@
 /**
  * malloc_checked-function that allocates memory using malloc
  * @b: size of memory to be allocated
- * Return:98 if malloc failes and a pointer to a space if it succeds
+ * Return: 98 if malloc failes and a pointer to a space if it succeds
  */
 
 void *malloc_checked(unsigned int b)
