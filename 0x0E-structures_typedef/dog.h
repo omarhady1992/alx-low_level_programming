a1fd63b6055c5a5118eb3c4b6933210e2d686cb2
root <ohady1992@gmail.com>
2023-07-17 14:53:56 -0700

    added a missing semi colon

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,15 +8,15 @@
  * @owner: dog owner's name
  */
 
-struct dog 
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}
+};
 
 /**
- * dog_t-Typedef for dog structure
+ * dog_t - typedef for struct dog
  */
 
 typedef struct dog dog_t;
@@ -25,5 +25,7 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
+char *cp_str(char *dest, char *src);
+int len_str(char *s);
 
 #endif
