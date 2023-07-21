4666710cc7a45a380f59e9f6ff011feb1d3fe870
root <ohady1992@gmail.com>
2023-07-21 14:46:01 -0700

    fixed file names

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "functions_pointers.h"
+#include <stdlib.h>
+
+/**
+ * print_name-prints a name
+ * @name: name to be printed
+ * @f: pointer to the printing function
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (!name || !f)
+		return;
+	f(name);
+}
