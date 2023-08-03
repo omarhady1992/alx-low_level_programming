a3e99f1595b84f6707f795ac7819004eb497df73
root <omarhady1992@gmail.com>
2023-08-03 14:13:28 -0700

    advanced tasks

---
+++ 0x14-bit_manipulation/100-get_endiannes.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * get_endianness - returns the endianness
+ * Return: 0 if big endian, 1 if small
+ */
+int get_endianness(void)
+{
+	unsigned long int n = 1;
+
+	return (*(char *)&n);
+}
